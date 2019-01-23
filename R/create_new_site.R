3dbdb4f8552eadb64aab7ee6ff2e9fb55ec2628e
David Von Lehman <david.vonlehman@gmail.com>
2019-01-22 16:15:56 -0800

    Read site_template parameter

--- R/create_new_site.R
+++ R/create_new_site.R
@@ -7,16 +7,13 @@
 #
 # The function itself just echos its inputs and outputs to a file called INDEX,
 # which is then opened by RStudio when the new project is opened.
-aerobatic_site <- function(path, ...) {
+aerobatic_site <- function(path, site_template) {
 
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
   # Download the GitHub repo corresponding to the selected site template
 
-  dots <- list(...)
-  website_template <- dots[[0]]
-
   # write to index file
-  writeLines(website_template, con = file.path(path, "index.Rmd"))
+  writeLines(site_template, con = file.path(path, "index.Rmd"))
 }
